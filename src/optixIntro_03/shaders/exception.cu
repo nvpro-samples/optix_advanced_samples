761724cee26239b8e43d27f045017210ba750264
droettger <droettger@nvidia.com>
2018-03-13 18:10:18 +0100

    Remove exception_tonemapper program.
    (Not needed before optixIntro_09.)

--- src/optixIntro_03/shaders/exception.cu
+++ src/optixIntro_03/shaders/exception.cu
@@ -31,8 +31,7 @@
 #include <optix.h>
 
 
-rtBuffer<float4,  2> sysOutputBuffer; // RGBA32F
-rtBuffer<float4,  2> sysTonemappedBuffer; // RGBA32F
+rtBuffer<float4, 2> sysOutputBuffer; // RGBA32F
 
 rtDeclareVariable(uint2, theLaunchIndex, rtLaunchIndex, );
 
@@ -52,21 +51,3 @@ RT_PROGRAM void exception()
   sysOutputBuffer[theLaunchIndex] = make_float4(1000000.0f, 0.0f, 1000000.0f, 1.0f);  // RGBA32F super magenta
 #endif
 }
-
-RT_PROGRAM void exception_tonemapper()
-{
-#if USE_DEBUG_EXCEPTIONS
-  const unsigned int code = rtGetExceptionCode();
-  if (RT_EXCEPTION_USER <= code)
-  {
-    rtPrintf("User exception %d at (%d, %d)\n", code - RT_EXCEPTION_USER, theLaunchIndex.x, theLaunchIndex.y);
-  }
-  else
-  {
-    rtPrintf("Exception code 0x%X at (%d, %d)\n", code, theLaunchIndex.x, theLaunchIndex.y);
-  }
-
-  sysTonemappedBuffer[theLaunchIndex] = make_float4(1000000.0f, 0.0f, 1000000.0f, 1.0f);  // RGBA32F super magenta
-#endif
-}
-
