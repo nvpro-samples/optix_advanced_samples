7f7fff8f0e4c2c21a975cba392625b9d5a67a780
droettger <droettger@nvidia.com>
2018-04-12 13:03:13 +0200

    Adding comment about the variable nomenclature used on device side.

--- src/optixIntroduction/optixIntro_01/shaders/raygeneration.cu
+++ src/optixIntroduction/optixIntro_01/shaders/raygeneration.cu
@@ -31,6 +31,15 @@
 #include <optix.h>
 #include <optixu/optixu_math_namespace.h>
 
+// Note, the nomenclature used in the device code of all optixIntroduction samples
+// follows some simple rules using prefixes to help indicating the scope and meaning:
+//
+// "sys" = renderer "system"-wide variables, defined at global context scope.
+// "the" = variables with OptiX built-in semantic, like rtLaunchIndex, etc.
+// "var" = "varyings" with developer defined attribute semantic, calculated by the intersection program.
+// "par" = "parameter" variable held at some object scope, not at the global context scope.
+//         (Exception to the last rule are the vertex "attributes" and "indices" held at Geometry nodes.)
+
 rtBuffer<float4, 2> sysOutputBuffer; // RGBA32F
 
 rtDeclareVariable(uint2, theLaunchIndex, rtLaunchIndex, );
